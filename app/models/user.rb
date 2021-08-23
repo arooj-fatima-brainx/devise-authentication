9921504ff0a7cd0b5e35cb6a09e1d4fa113c31c9
arooj-fatima-brainx <arooj.fatima@brainxtech.com>
2021-08-23 18:13:59 +0500

    DEVISE Authentication(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,  :recoverable, :rememberable, :validatable
 
   # devise :database_authenticatable, :registerable, :confirmable, :recoverable, stretches: 13
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,32 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  resources :users
+
+
+   # devise_for :users
+  #   devise_for :users, controllers: { sessions: 'users/sessions' }
+  #
+  #  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
+
+   # devise_for :users, skip: :all
+   # devise_for :users, :skip => :registration
+
   root to: "users#index"
+
+   devise_for :users, :skip => [:registrations], controllers: {
+     sessions: 'users/sessions'
+   }
+
+   # devise_scope :user do
+   #   get "/sign_in" => "devise/sessions#new"
+   #   # get "/sign_up" => "devise/registrations#new"
+   # end
+
+   resources :users
+  # devise_scope :user do
+  #   get 'sign_in', to: 'devise/sessions#new'
+  # end
+
 
   # root to: 'home#index'
 
